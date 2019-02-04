907494e8a70c462f388550231541aa80c9798311
paigewalker <paigemariewalker@gmail.com>
2019-02-04 14:42:16 -0500

    Adds padded margins to born-digital Heights files

---
+++ heights-add-margins.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# This script adds small white margins around tightly cropped Heights files 
+# Use it after you convert Heights PDFs to individual TIFs and rename the files
+# All TIFs should be in the same directory, which should be spelled out in 'list='
+# Dependencies: ImageMagick
+
+cd
+list=`find /Volumes/Digital-Library/current-projects/born-digital_BC-pubs/heights-working -type d`
+echo "list=$list"
+for directory in $list; do
+echo "directory=$directory"
+cd $directory
+mogrify -format tif -gravity center -background white -extent 110x104% *.tif
+cd
+done
